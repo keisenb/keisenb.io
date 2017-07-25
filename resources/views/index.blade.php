689b371c2f42aa91132d25f4b6d5521e4fb6eb68
Kyle Eisenbarger <keisenb@ksu.edu>
2017-07-25 16:31:34 -0500

    Updated page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,15 @@
 
 
 
+<!--<div class="container">
+<div class="jumbotron jmbo">
+  <h1 class="name">Kyle Eisenbarger</h1>
+  <p>...</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+</div>-->
+
+
 <div id="bgndVideo" class="player" data-property="{videoURL:'https://youtu.be/a4OikKyIT_A',containment:'body',autoPlay:true, loop:true, mute:true, showControls:false, quality:'hd1080'}"></div>
 
 @endsection
