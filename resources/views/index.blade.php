9da732a8ab8d49f61dddc9e6ec23ad6545e3e384
Kyle Eisenbarger <keisenb@ksu.edu>
2017-08-08 17:33:53 -0500

    Removed Laravel, Updated site

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,18 +4,25 @@
 @section('content')
 
 
+<div class="container">
+    <h1>Kyle Eisenbarger</h1>
+    <h4>Full Stack Developer</h4>
+    <div class="social">
+       <a href="https://twitter.com/hackkstate" target="_blank"><i class="fa fa-github fa-lg" aria-hidden="true"></i></a>
+       <a href="https://twitter.com/hackkstate" target="_blank"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a>
+       <a href="https://twitter.com/hackkstate" target="_blank"><i class="fa fa-snapchat fa-lg" aria-hidden="true"></i></a>
+       <a href="https://www.facebook.com/hackkstate/" target="_blank"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a>
+       <a href="https://www.instagram.com/hackkstate/" target="_blank"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i></a>
+       <a href="https://twitter.com/hackkstate" target="_blank"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a>
+       <a href="https://twitter.com/hackkstate" target="_blank"><i class="fa fa-twitch fa-lg" aria-hidden="true"></i></a>
+       <a href="https://twitter.com/hackkstate" target="_blank"><i class="fa fa-steam fa-lg" aria-hidden="true"></i></a>
 
 
-<!--<div class="container">
-<div class="jumbotron jmbo">
-  <h1 class="name">Kyle Eisenbarger</h1>
-  <p>...</p>
-  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-</div>
-</div>-->
+   </div>
+ </div>
 
+ <div id="bgndVideo" class="player" data-property="{videoURL:'https://youtu.be/a4OikKyIT_A',containment:'body',autoPlay:true, loop:true, mute:true, showControls:false, opacity:0.5 , quality:'hd1080'}"></div>
 
-<div id="bgndVideo" class="player" data-property="{videoURL:'https://youtu.be/a4OikKyIT_A',containment:'body',autoPlay:true, loop:true, mute:true, showControls:false, quality:'hd1080'}"></div>
 
 @endsection
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,41 +14,7 @@
   <link href="{{ URL::asset('css/main.css') }}" type="text/css" rel="stylesheet" media="screen,projection" />
 
   @yield('head')
-</head>
-    <body>
-        <div class="container">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="{{ url('/') }}">Kyle Eisenbarger</a>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="{{ url('/') }}">Home</a></li>
-                            <li><a href="{{ url('/about') }}">About</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projects <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="http://testing.kyle-eisenbarger.com/" target="_blank">Testing</a></li>
-                                    <li><a href="{{ url('/drone') }}">Drone</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li class="dropdown-header">Websites</li>
-                                    <li><a href="#" target="_blank">Atrium Display</a></li>
-                                    <li><a href="#" target="_blank">K-State ACM</a></li>
-                                </ul>
-                                <li><a href="{{ url('/contact') }}">Contact</a></li>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+
 
 
 
